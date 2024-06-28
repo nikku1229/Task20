034ff6e3cf603d97552aae16109d4a7004995565
Nitesh Sharma <147125201+nikku1229@users.noreply.github.com>
2024-06-28 21:39:07 +0530

    ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./list.css";
+import SmallList from "./SmallList";
 
 function List({ listItems,increaseQty,decreaseQty,totalPrice }){
   return (
@@ -26,31 +27,4 @@ function List({ listItems,increaseQty,decreaseQty,totalPrice }){
   );
 }
 
-function SmallList({item,index,increaseQty,decreaseQty}) {
-  let [itemQtys, setItemQtys] = useState(1);
-
-
-  return (
-    <li>
-      <div className="product-list-img">
-        <img src={item.image} alt="" />
-      </div>
-      <div className="product-detail">
-        <h4>{item.title}</h4>
-        <p>
-          $<span>{item.price}</span>
-        </p>
-      </div>
-      <div className="btns">
-        <button className="remove-btn" onClick={() => decreaseQty(index,itemQtys,setItemQtys,item.price)}>
-          <ion-icon name="remove-outline"></ion-icon>
-        </button>
-        <span>{itemQtys}</span>
-        <button className="add-btn" onClick={() => increaseQty(itemQtys,setItemQtys,item.price)}>
-          <ion-icon name="add-outline"></ion-icon>
-        </button>
-      </div>
-    </li>
-  );
-}
 export default List;
